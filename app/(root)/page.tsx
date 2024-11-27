dff85389157eec7e24b8f5e0b5d93442837987a1
JamesGabby <jlghfe36@gmail.com>
2024-11-27 15:25:40 +0000

    create input comp

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
+import SearchForm from "../components/SearchForm";
 
 export default function Home() {
   return (
     <>
       <section className="pink_container">
+        <p className="tag">Pitch, Vote, And Grow</p>
+
         <h1 className="heading">
           Pitch Your Startup, <br />
           Connect With Entrepreneurs
@@ -13,6 +16,8 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, 
           and Get Noticed in Virtual Competitions.
         </p>
+
+        <SearchForm />
       </section>
       
     </>
