b21f8e0b3275f4a0cd933c18479db03cf558a43f
JamesGabby <jlghfe36@gmail.com>
2024-11-29 01:39:45 +0000

    implement form logic for search n home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
 import SearchForm from "../components/SearchForm";
 
-export default function Home() {
+export default async function Home({ searchParams }: {
+  searchParams: Promise<{ query?: string }>
+}) {
+  const query = (await searchParams).query
+
   return (
     <>
       <section className="pink_container">
@@ -17,7 +20,7 @@ export default function Home() {
           and Get Noticed in Virtual Competitions.
         </p>
 
-        <SearchForm />
+        <SearchForm query={query} />
       </section>
       
     </>
