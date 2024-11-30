961204e2072b1d240ce0ee4ec2fad57f960d3fb1
JamesGabby <jlghfe36@gmail.com>
2024-11-30 02:05:30 +0000

    destructure props coming from post

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,25 +3,36 @@ import { EyeIcon } from "lucide-react"
 import Link from "next/link"
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
+  const { 
+    _createdAt, 
+    views, 
+    author: { 
+      _id: authorId, 
+      name 
+    }, 
+    _id, description, image, category, 
+    title 
+  } = post
+
   return (
     <li className="startup-card group">
       <div className="flex-between">
         <p className="startup_card_date">
-          {formatDate(post._createdAt)}
+          {formatDate(_createdAt)}
         </p>
         <div className="flex gap-1.5">
           <EyeIcon className="size-6 text-primary" />
           <span className="text-16-medium">
-            {post.views}
+            {views}
           </span>
         </div>
       </div>
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${post.author?._id}`}>
+          <Link href={`/user/${authorId}`}>
             <p className="text-16-medium line-clamp-1">
-              {post.author?.name}
+              {name}
             </p>
           </Link>
         </div>
