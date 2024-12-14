9014104ff1dc251193de3d64b52b10b9cfe741dc
JamesGabby <jlghfe36@gmail.com>
2024-12-14 21:49:57 +0000

    create ping and render view/s depending on number of views

--- components/View.tsx
+++ components/View.tsx
@@ -1,5 +1,6 @@
 import { fetchStartupById } from '@/lib/supabase';
 import React from 'react'
+import Ping from './Ping';
 
 const View = async ({ id }: { id: string }) => {
   const { data: startup, error } = await fetchStartupById(id);
@@ -8,8 +9,19 @@ const View = async ({ id }: { id: string }) => {
     console.error('Error fetching startup:', error.message, error.details);
     return <div>Error loading startup. Please try again later.</div>;
   }
+
   return (
-    <div>Views: {startup.views}</div>
+    <div className="view-container">
+      <div className="absolute -top-2">
+        <Ping />
+      </div>
+
+      <p className="view-text">
+        <span className="font-black">
+          {startup.views} {startup.views > 1 ? 'views' : 'view'}
+        </span>
+      </p>
+    </div>
   )
 }
 
