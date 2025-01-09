7da4f1cd8cef1002d012434ca1f9ae4dbafc417f
JamesGabby <jlghfe36@gmail.com>
2025-01-09 15:05:06 +0000

    start building out startup form

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -1,6 +1,21 @@
+'use client'
+
+import { Input } from "./ui/input"
+
 const StartupForm = () => {
   return (
-    <div>StartupForm</div>
+    <form action={() => {}} className="startup-form">
+      <label htmlFor="title" className="startup-form_label">
+        Title
+      </label>
+      <Input 
+        id="title"
+        name="title"
+        className="startup-form_input"
+        required
+        placeholder="Startup Title"
+      />
+    </form>
   )
 }
 
